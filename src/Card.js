38b5824dae83dbf2957b97ac6e695ff061f99f89
gogiabu <146556307+gogiabu@users.noreply.github.com>
2024-10-17 10:11:50 -0700

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -8,7 +8,7 @@ function Card() {
             <img className='image' src={profilepic} alt="card image"></img>
             <h2 className='title'>Shawarma City</h2>
             <p className='text'>ბათუმში უგემრიელეს შაურმას მიირთმევთ ჩვენთან.
-                <br></br><br></br> <i>"თუ შეგაწუხა კუჭის ხმაურმა მაშინ, მიირთვი ჩვენი შაურმა"</i>.<br></br><br></br>
+                <br></br><br></br> <i>"თუ შეგაწუხა კუჭის ხმაურმა, მაშინ მიირთვი ჩვენი შაურმა"</i>.<br></br><br></br>
                 <b>მის: ი. ჭავჭავაძის 49.(26 მაისის და ჭავჭავაძის კვეთა)</b> </p>
 
         </div>
